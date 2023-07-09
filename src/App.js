357a17e29e5e4744d470054e37b530e8abbfb017
JudeaDumont <dumontjudea2@gmail.com>
2023-07-09 11:51:16 -0600

    - Added error UI element for when the API is not running and contacts ccould not be retrieved on initial render

--- src/App.js
+++ src/App.js
@@ -1,63 +1,74 @@
 import Axios from 'axios';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import ContactDataGrid from './ContactDataGrid.tsx';
 import BASE_URL from './global-variables.tsx';
 
 function App() {
 
-  const [name, setName] = useState("");
-  const [id, setID] = useState("");
+    const [name, setName] = useState("");
+    const [id, setID] = useState("");
+    const [error, setError] = useState("");
 
-  const getContacts = () => {
-    Axios.get(BASE_URL + '/api/Contacts')
-    .then(resp => setContacts([...resp.data]));
-  }
+    const getContacts = () => {
+        Axios.get(BASE_URL + '/api/Contacts')
+            .then(resp => setContacts([...resp.data]))
+            .catch(error => {
+                
+                let message = "Could not retrieve contacts: " + error.message + ", is the API running?";
+                console.log(message);
+                setError(message);
+            });
+    }
 
-  const addContact = () => {
-    Axios.post(BASE_URL + '/api/Contacts', {
-      name: name
-    });
-    getContacts(); //todo: the reason why the table does not immediately update is becuase database does not instantly write it!
-  }
+    const addContact = () => {
+        Axios.post(BASE_URL + '/api/Contacts', {
+            name: name
+        });
+        getContacts(); //todo: the reason why the table does not immediately update is becuase database does not instantly write it!
+    }
 
-  const deleteById = () => {
-    Axios.delete(BASE_URL + '/api/Contacts/' + id);
-    getContacts();
-  }
+    const deleteById = () => {
+        Axios.delete(BASE_URL + '/api/Contacts/' + id);
+        getContacts();
+    }
 
-  const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([]);
 
-  useEffect(() => {
-    getContacts();
-  }, []);
+    useEffect(() => {
+        getContacts();
+    }, []);
 
-  return (
-    <div className="App">
-      <ContactDataGrid contacts={contacts}/>
+    return (
+        <div className="App">
+            { error &&
+                <h3 className="error"> { error } </h3> }
+            <ContactDataGrid contacts={contacts}/>
 
-      <label>Name:</label>
-      <input type="text"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-        id="name-input" />
-      <button
-        onClick={addContact}
-        id="add-candidate-button">Add</button>
+            <label>Name:</label>
+            <input type="text"
+                   onChange={(event) => {
+                       setName(event.target.value);
+                   }}
+                   id="name-input"/>
+            <button
+                onClick={addContact}
+                id="add-candidate-button">Add
+            </button>
 
-      <p></p>
-      <label>ID:</label>
-      <input type="text"
-        onChange={(event) => {
-          setID(event.target.value);
-        }}
-        id="id-input" />
-      <button
-        onClick={deleteById}
-        id="delete-candidate-button">Delete</button>
-    </div>
-  );
+            <p></p>
+            <label>ID:</label>
+            <input type="text"
+                   onChange={(event) => {
+                       setID(event.target.value);
+                   }}
+                   id="id-input"/>
+            <button
+                onClick={deleteById}
+                id="delete-candidate-button">Delete
+            </button>
+        </div>
+    );
 }
 
 export default App;
